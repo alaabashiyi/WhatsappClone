edb51db429db9712e54a0fc352f2b2ffced54d72
Alaa Bashiyi <alaabashiyi@Alaas-iMac.local>
2020-12-13 02:49:36 +0200

    fetch and display messages

--- screens/ChatRoomScreen.tsx
+++ screens/ChatRoomScreen.tsx
@@ -1,21 +1,49 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Text, FlatList, ImageBackground } from "react-native";
 
 import { useRoute } from "@react-navigation/native";
 
-import chatRoomData from "../data/Chats";
 import ChatMessage from "../components/ChatMessage";
 import BG from "../assets/images/BG.png";
 import InputBox from "../components/InputBox";
+import { API, Auth, graphqlOperation } from "aws-amplify";
+import { messagesByChatRoom } from "../graphql/queries";
 
 const ChatRoomScreen = () => {
   const route = useRoute();
+  const [messages, setMessages] = useState([]);
+  const [myId, setMyId] = useState(null);
+
+  useEffect(() => {
+    const fetchMessages = async () => {
+      const messagesData = await API.graphql(
+        graphqlOperation(messagesByChatRoom, {
+          chatRoomID: route.params.id,
+          sortDirection: "DESC",
+        })
+      );
+
+      setMessages(messagesData.data.messagesByChatRoom.items);
+    };
+
+    fetchMessages();
+  });
+
+  useEffect(() => {
+    const getMyId = async () => {
+      const userInfo = await Auth.currentAuthenticatedUser();
+
+      setMyId(userInfo.attributes.sub);
+    };
+
+    getMyId();
+  });
 
   return (
     <ImageBackground source={BG} style={{ width: "100%", height: "100%" }}>
       <FlatList
-        data={chatRoomData.messages}
-        renderItem={({ item }) => <ChatMessage message={item} />}
+        data={messages}
+        renderItem={({ item }) => <ChatMessage myId={myId} message={item} />}
         inverted
       />
 
